a7f45b928c0155251c4b6854e961f26a00addb08
Vijay14958 <116162193+Vijay14958@users.noreply.github.com>
2022-12-20 19:24:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/Vijay14958/Mdiskautofilter-V5.git /Mdiskautofilter-V5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5
+  git clone $UPSTREAM_REPO /Mdiskautofilter-V5
 fi
-cd /Auto-Filter-V5
+cd /Mdiskautofilter-V5
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
