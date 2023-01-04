ef1729fba46250e0c8c9e3eee5f001e98ed166c9
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-01-04 15:24:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vjmovierequest:vjmovierequest@cluster0.lthu1hh.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mdisk123:Mdisk123@cluster0.hx59djt.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "TechVJv5Bot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
