f28a0baee479e7783708d5b12a265c293addc364
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-01-07 14:34:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001855793002))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001855754121))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/Tech_VJ_File')
-HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/TechVJDownload')
+HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://youtube.com/shorts/aGTu09WWUcw?feature=share')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001834678099))
