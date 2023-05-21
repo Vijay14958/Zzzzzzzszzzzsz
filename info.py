8673618d206d2f2cf03d928fd3eef0f00f4a7802
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-05-21 06:44:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mdisk123:Mdisk123@cluster0.hx59djt.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "TechVJv5Bot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Forward123:Forward123@cluster0.4d1ljfv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "TechVJv5Botsss")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filess')
 
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/VJ_Bot_Disscussion')
